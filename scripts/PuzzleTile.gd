2bdffb5421bb1e1e21e74075d7feaca89da1d50d
Mark Ross <markross470@gmail.com>
2022-10-18 17:39:58 +0100

    Made cells rotate when clicked

--- scripts/PuzzleTile.gd
+++ scripts/PuzzleTile.gd
@@ -1,5 +1,4 @@
-extends Node
-
+extends Spatial
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -11,8 +10,15 @@ var mat_override: Material
 
 func mouse_enter():
 	mat_override.set_shader_param("main_colour", Color(1, 0, 0))
+	
 func mouse_exit():
 	mat_override.set_shader_param("main_colour", Color(0, 1, 0))
+	
+func input_event(camera: Node, event: InputEvent, position: Vector3, normal: Vector3, shape_idx: int):
+	if event is InputEventMouseButton:
+		if event.button_index == 1 and event.pressed:
+			print("Clicked")
+			rotate(Vector3.FORWARD, PI / 2)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -24,3 +30,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+
