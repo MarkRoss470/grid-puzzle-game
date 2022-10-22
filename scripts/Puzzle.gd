b5fee1fcf836794fea47e94c0e26c3ebfb265a1b
Mark Ross <markross470@gmail.com>
2022-10-22 15:18:23 +0100

    Added basic puzzle functionality

--- scripts/Puzzle.gd
+++ scripts/Puzzle.gd
@@ -9,21 +9,51 @@ class_name Puzzle
 	[[PuzzleEdge or null; width + 1]; height + 1] #vertical edges
 ]"""
 export(Array) var puzzle
+export(Color) var colour_base = Color(0.5, 0.5, 0.5)
+export(Color) var colour_hover = Color(0.8, 0.8, 0.8)
+export(Color) var colour_on_incorrect = Color(1, 0, 0)
+export(Color) var colour_solved_base = Color(0, 1, 0)
+export(Color) var colour_solved_hover = Color(0.5, 1, 0.5)
 
+# What object to instance as a tile
 export(NodePath) var instance
+# What object to call the on_puzzle_solve method of when the puzle is solved
 export(NodePath) var on_complete
+# What parameter to pass to on_puzzle_solve
+export(int) var on_complete_param
+
+# Array[x][y] of the direction of cells
+# 0 = up, 1 = right etc
+var current_state: Array
+# Stores references to the PuzzleTile nodes of this puzzle
+var tiles: Array
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# Loop over cells in puzzle
 	for x in range(puzzle[PuzzleClasses.WIDTH]):
+		current_state.append([])
+		tiles.append([])
 		for y in range(puzzle[PuzzleClasses.HEIGHT]):
+			# Initialise current puzzle state
+			current_state[x].append(0)
+			# Create new tile
 			var tile = create_tile(x, y, puzzle[PuzzleClasses.CELLS][x][y])
 			tile.transform.origin = Vector3(x, -y, 0)
+			tiles[x].append(tile)
 			add_child(tile)
-	pass # Replace with function body.
 
-func create_tile(_x, _y, cell) -> Spatial:
-	var node: Spatial = get_node(instance).duplicate()
+# Creates a new puzzle cell object
+func create_tile(x: int, y: int, cell) -> PuzzleTile:
+	var node: PuzzleTile = get_node(instance).duplicate()
+	
+	node.colour_hover = colour_hover
+	node.colour_base = colour_base
+	
+	node.this_x = x
+	node.this_y = y
+	node.puzzle = self
 	node.set_visible(true)
 	var icon: CSGMesh = node.get_node("Icon")
 	if cell != null:
@@ -36,6 +66,34 @@ func create_tile(_x, _y, cell) -> Spatial:
 	node.rotate(Vector3.RIGHT, PI / 2)
 	return node
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# Called by a cell when it is clicked
+func rotate_cell(x, y):
+	# Add one to cell's rotation
+	current_state[x][y] += 1
+	# Wrap around to 0 if reaches 4
+	current_state[x][y] %= 4
+	
+# Checks whether the current solution is valid
+# Calls on_complete.on_puzzle_solve(on_complete_param) if it is
+func check_solution():
+	var solved := true
+	# Initialise the cells to the base colour
+	for column in tiles:
+			for tile in column:
+				tile.set_colour(colour_base, colour_hover)
+	
+	
+	if current_state[0][0] == 1:
+		solved = false
+	
+	#TODO: check whether the puzzle is actually solved
+	var node = get_node(on_complete)
+	if solved:
+		for column in tiles:
+			for tile in column:
+				tile.set_colour(colour_solved_base, colour_solved_hover)
+		
+		if node != null:
+			node.on_puzzle_solve(on_complete_param)
+	elif node != null:
+		node.on_puzzle_unsolve(on_complete_param)
--- scripts/PuzzleTile.gd
+++ scripts/PuzzleTile.gd
@@ -1,31 +1,57 @@
 extends Spatial
 
+class_name PuzzleTile
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
 export(NodePath) var backplane_path
+
+var colour_base: Color
+var colour_hover: Color
+
 var backplane: CSGMesh
 var mat_override: Material
 
+var this_x: int
+var this_y: int
+var puzzle
+
+var mouse_over_tile: bool = false
+
+func set_colour(colour: Color, colour_h: Color):
+	colour_base = colour
+	colour_hover = colour_h
+	
+	if mouse_over_tile:
+		mat_override.set_shader_param("main_colour", colour_hover)
+	else:
+		mat_override.set_shader_param("main_colour", colour_base)
+
 func mouse_enter():
-	mat_override.set_shader_param("main_colour", Color(1, 0, 0))
+	mouse_over_tile = true
+	mat_override.set_shader_param("main_colour", colour_hover)
 	
 func mouse_exit():
-	mat_override.set_shader_param("main_colour", Color(0, 1, 0))
-	
-func input_event(camera: Node, event: InputEvent, position: Vector3, normal: Vector3, shape_idx: int):
+	mouse_over_tile = false
+	mat_override.set_shader_param("main_colour", colour_base)
+
+func input_event(_camera: Node, event: InputEvent, _position: Vector3, _normal: Vector3, _shape_idx: int):
 	if event is InputEventMouseButton:
 		if event.button_index == 1 and event.pressed:
 			print("Clicked")
+			puzzle.rotate_cell(this_x, this_y)
 			rotate(Vector3.FORWARD, PI / 2)
+			
+			get_node("../../Player").most_recent_puzzle = puzzle
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	backplane = get_node(backplane_path)
 	mat_override = backplane.get_material().duplicate()
 	backplane.set_material_override(mat_override)
-
+	mat_override.set_shader_param("main_colour", colour_base)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
