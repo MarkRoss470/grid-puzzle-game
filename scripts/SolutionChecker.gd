2d36488194d77e410c7d4215457826fb50f8129e
Mark Ross <markross470@gmail.com>
2022-10-24 22:04:21 +0100

    Added checks for pointer cell icons

--- scripts/SolutionChecker.gd
+++ scripts/SolutionChecker.gd
@@ -12,12 +12,81 @@ class Solution:
 	var wrong_cells: Array
 	
 	# Simple constructor
-	func _init(c: bool, w: Array = []):
-		self.is_correct = c
-		self.wrong_cells = w
+	func _init():
+		is_correct = true
+		wrong_cells = []
+	
+	# Add a wrong cell and set is_correct to false
+	func add_wrong(cell: Array):
+		is_correct = false
+		wrong_cells.append(cell)
 
-# TODO: check actual solutions
+# ------------RULES------------
+# Pointers: The edge being pointed to must have a bar on at least one side
+
 static func check_solution(puzzle: Array, solution: Array) -> Solution:
-	if solution[0][0] == 1:
-		return Solution.new(false, [[0, 0]])
-	return Solution.new(true)
+	# A Solution to add results into
+	var result = Solution.new()
+
+	# Get which edges have a bar on at least one side
+	var edges := get_filled_edges(solution)
+
+	# Loop over all cells and check whether the rules are followed
+	for x in range(len(solution)):
+		for y in range(len(solution[0])):
+			match puzzle[PuzzleClasses.CELLS][x][y][0]:
+				# Pointer cells: check the right edge to see if there is a bar there
+				PuzzleClasses.POINTER_UP:
+					if not edges.horizontal[x][y]:
+						result.add_wrong([x, y])
+				PuzzleClasses.POINTER_DOWN:
+					if not edges.horizontal[x][y + 1]:
+						result.add_wrong([x, y])
+				PuzzleClasses.POINTER_LEFT:
+					if not edges.vertical[x][y]:
+						result.add_wrong([x, y])
+				PuzzleClasses.POINTER_RIGHT:
+					if not edges.vertical[x + 1][y]:
+						result.add_wrong([x, y])
+
+	return result
+
+class FilledEdges:
+	# The horizontal edges
+	# bool[x][y + 1]
+	var horizontal: Array
+	# The vertical edges
+	# bool[x + 1][y]
+	var vertical: Array
+	func _init(x: int, y: int):
+		# Initiase horizontal with the right dimensions
+		horizontal = []
+		for i in range(x):
+			horizontal.append([])
+			for _j in range(y + 1):
+				horizontal[i].append(false)
+		# Initiase vertical with the right dimensions
+		vertical = []
+		for i in range(x + 1):
+			vertical.append([])
+			for _j in range(y):
+				vertical[i].append(false)
+
+# Takes a puzzle solution and returns which edges have a bar on at least one side of them
+static func get_filled_edges(solution: Array) -> FilledEdges:
+	# A FilledEdges object to add data to
+	var edges := FilledEdges.new(len(solution), len(solution[0]))
+	# For each cell, set the edge the right side of it to true
+	for x in range(len(solution)):
+		for y in range(len(solution[0])):
+			match solution[x][y]:
+				0: # Up
+					edges.horizontal[x][y] = true
+				1: # Right
+					edges.vertical[x + 1][y] = true
+				2: # Down
+					edges.horizontal[x][y + 1] = true
+				3: # Left
+					edges.vertical[x][y] = true
+
+	return edges
