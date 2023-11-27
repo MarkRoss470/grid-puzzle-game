76b6ae26d19e6344824827dfcb8bd17018487156
Mark Ross <markross470@gmail.com>
2023-11-27 15:20:32 +0000

    Removed SingleSolutionEditor

--- addons/PuzzleEditorPlugin/PuzzleEditorInspectorPlugin.gd
+++ addons/PuzzleEditorPlugin/PuzzleEditorInspectorPlugin.gd
@@ -6,12 +6,11 @@ class_name PuzzleEditorInspectorPlugin
 
 # Editor to load
 var PuzzleEditor := preload("res://addons/PuzzleEditorPlugin/PuzzleEditor.gd")
-var SingleSolutionEditor := preload("res://addons/PuzzleEditorPlugin/SingleSolutionEditor.gd")
 
 # Checks whether to check the properties of an object
 func _can_handle(object: Object) -> bool:
 	# Check type of object - only load editor into nodes of type Puzzle
-	return object is PuzzleDesign or object is SingleSolutionPuzzle
+	return object is PuzzleDesign
 
 func _parse_begin(object):
 	if object is PuzzleDesign:
@@ -20,6 +19,3 @@ func _parse_begin(object):
 			["width", "height", "icons"],
 			PuzzleEditor.new()
 		)
-	
-	if object is SingleSolutionPuzzle:
-		add_custom_control(SingleSolutionEditor.new())
